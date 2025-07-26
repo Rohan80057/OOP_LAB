44cb7413a57e94179ef57d4a96a986be6e84b7d2
Rohan80057 <2420080057@klh.edu.in>
2025-07-26 09:15:12 +0530

    Updated-25july

---
+++ char_is_alphabet.java
@@ -0,0 +1,18 @@
+package oop_lab;
+import java.util.Scanner;
+public class char_is_alphabet {
+	public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a character:");
+		char ch =sc.next().charAt(0);
+		if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
+			System.out.println(ch +"is a alphabet");
+		} else {
+			System.out.println(ch +"is not a alphabet");
+			
+		}
+		
+    }
+
+}
